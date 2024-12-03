feeaa47ae14999e6198752ba2a182a4384bfb9fa
jls <jls@cordulus.com>
2024-12-03 09:32:12 +0100

    Solve 02.b
    Very crude brute force implementation. This is probably the slowest
    implementation you could make.

--- 2-python/main.py
+++ 2-python/main.py
@@ -3,12 +3,17 @@ import unittest
 
 def main():
     safe = 0
+    safe_with_single_replacement = 0
     with open("input.txt") as f:
         for line in f:
             numbers = [int(x) for x in line.split(" ")]
             if is_safe(numbers):
                 safe += 1
+            if is_safe_with_single_remove(numbers):
+                safe_with_single_replacement += 1
     print(safe)
+
+    print(safe_with_single_replacement)
 
 
 def is_safe(numbers: list[int]) -> bool:
@@ -33,6 +38,17 @@ def is_safe(numbers: list[int]) -> bool:
     return True
 
 
+def is_safe_with_single_remove(numbers: list[int]) -> bool:
+    if is_safe(numbers):
+        return True
+
+    for i in range(len(numbers)):
+        sliced = numbers[:i] + numbers[i + 1 :]
+        if is_safe(sliced):
+            return True
+    return False
+
+
 class TestIsSafe(unittest.TestCase):
     def test_is_safe(self):
         examples = [
@@ -46,6 +62,21 @@ class TestIsSafe(unittest.TestCase):
 
         for numbers, expected in examples:
             self.assertEqual(is_safe(numbers), expected, f"Failing for {numbers}")
+
+    def test_is_safe_with_single_remove(self):
+        examples = [
+            ([7, 6, 4, 2, 1], True),
+            ([1, 2, 7, 8, 9], False),
+            ([9, 7, 6, 2, 1], False),
+            ([1, 3, 2, 4, 5], True),
+            ([8, 6, 4, 4, 1], True),
+            ([1, 3, 6, 7, 9], True),
+        ]
+
+        for numbers, expected in examples:
+            self.assertEqual(
+                is_safe_with_single_remove(numbers), expected, f"Failing for {numbers}"
+            )
 
 
 if __name__ == "__main__":
