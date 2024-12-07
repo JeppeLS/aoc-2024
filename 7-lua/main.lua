71c6c872dd26d4064b23ff4ca8ceb62439176d11
jls <jls@cordulus.com>
2024-12-07 15:34:14 +0100

    Solve 07.b

--- 7-lua/main.lua
+++ 7-lua/main.lua
@@ -32,7 +32,7 @@ local function pop_immutable(arr)
     return first, rest
 end
 
-local function is_solvable_recursive(result, current, numbers)
+local function is_solvable_recursive(result, current, numbers, enable_concat)
     if current > result then
         return false
     end
@@ -42,38 +42,50 @@ local function is_solvable_recursive(result, current, numbers)
 
     local next, rest = pop_immutable(numbers)
 
+    if enable_concat then
+        local digets = math.ceil(math.log(next + 1, 10))
+        local concat = current * math.floor(10 ^ digets) + next
+        if is_solvable_recursive(result, concat, rest, enable_concat) then
+            return true
+        end
+    end
     local multiplied = current * next
-    if is_solvable_recursive(result, multiplied, rest) then
+    if is_solvable_recursive(result, multiplied, rest, enable_concat) then
         return true
     end
-
     local added = current + next
-    if is_solvable_recursive(result, added, rest) then
+    if is_solvable_recursive(result, added, rest, enable_concat) then
         return true
     end
     return false
 end
 
-local function is_solvable(result, numbers)
+local function is_solvable(result, numbers, enable_concat)
     local first, rest = pop_immutable(numbers)
 
-    return is_solvable_recursive(result, first, rest)
+    return is_solvable_recursive(result, first, rest, enable_concat)
 end
 
 local input = read_lines("input.txt")
 
-local function part_one(lines)
-    local res = 0
+local function main(lines)
+    local p1 = 0
+    local p2 = 0
 
     for _, line in pairs(lines) do
         local result, numbers = split_result_and_parts(line)
 
-        if is_solvable(result, numbers) then
-            res = res + result
+        if is_solvable(result, numbers, false) then
+            p1 = p1 + result
+            -- Also solvable with concat
+            p2 = p2 + result
+        elseif is_solvable(result, numbers, true) then
+            p2 = p2 + result
         end
     end
-    return res
+    return p1, p2
 end
 
-local res = part_one(input)
-print(res)
+local p1, p2 = main(input)
+print("Part 1: ", p1)
+print("Part 2: ", p2)
