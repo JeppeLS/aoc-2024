a23a1eaa1be4a36655cdbfda8eecc1e364465682
jls <jls@cordulus.com>
2024-12-10 20:57:04 +0100

    Solve 10.b

--- 10-nim/main.nim
+++ 10-nim/main.nim
@@ -29,14 +29,34 @@ proc find_trail_heads(lines: seq[string], i: int, j: int): HashSet[Coord] =
     if j < lines[0].len() - 1 and lines[i][j + 1] == next:
         result.incl(find_trail_heads(lines, i, j + 1))
 
+proc find_trail_ratings(lines: seq[string], i: int, j: int): int =
+    let current = lines[i][j]
+    if current == '9':
+        return 1
+
+    result = 0
+    let next = char(int(current) + 1)
+    if i > 0 and lines[i - 1][j] == next:
+        result += find_trail_ratings(lines, i - 1, j)
+    if i < lines.high() and lines[i + 1][j] == next:
+        result += find_trail_ratings(lines, i + 1, j)
+    if j > 0 and lines[i][j - 1] == next:
+        result += find_trail_ratings(lines, i, j - 1)
+    if j < lines[0].len() - 1 and lines[i][j + 1] == next:
+        result += find_trail_ratings(lines, i, j + 1)
+
 var trailhead_sum = 0
+var trail_rating_sum = 0
 for i in 0 ..< size:
     for j in 0 ..< width:
         let c = lines[i][j]
         if c != '0':
             continue
         let trailheads = find_trailheads(lines, i, j)
+        let trail_ratings = find_trail_ratings(lines, i, j)
         trailhead_sum += trailheads.len()
+        trail_rating_sum += trail_ratings
 
 
 echo "Trailheads: ", trailhead_sum
+echo "Trail ratings: ", trail_rating_sum
