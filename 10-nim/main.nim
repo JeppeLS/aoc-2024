2c0bb59b8cc361a42c1a396a1ae1db7c8056528a
jls <jls@cordulus.com>
2024-12-10 20:50:48 +0100

    Solve 10.a

---
+++ 10-nim/main.nim
@@ -0,0 +1,42 @@
+import std/strutils
+import std/sequtils
+import std/sets
+
+type
+    Coord = object
+        i, j: int
+
+let input = readFile("input.txt")
+let lines = input.splitLines().filterIt(it.len() > 0)
+
+let size = lines.len()
+let width = lines[0].len()
+
+proc find_trail_heads(lines: seq[string], i: int, j: int): HashSet[Coord] =
+    result = initHashSet[Coord]()
+    let current = lines[i][j]
+    if current == '9':
+        let trailhead = Coord(i: i, j: j)
+        result.incl(trailhead)
+
+    let next = char(int(current) + 1)
+    if i > 0 and lines[i - 1][j] == next:
+        result.incl(find_trail_heads(lines, i - 1, j))
+    if i < lines.high() and lines[i + 1][j] == next:
+        result.incl(find_trail_heads(lines, i + 1, j))
+    if j > 0 and lines[i][j - 1] == next:
+        result.incl(find_trail_heads(lines, i, j - 1))
+    if j < lines[0].len() - 1 and lines[i][j + 1] == next:
+        result.incl(find_trail_heads(lines, i, j + 1))
+
+var trailhead_sum = 0
+for i in 0 ..< size:
+    for j in 0 ..< width:
+        let c = lines[i][j]
+        if c != '0':
+            continue
+        let trailheads = find_trailheads(lines, i, j)
+        trailhead_sum += trailheads.len()
+
+
+echo "Trailheads: ", trailhead_sum
