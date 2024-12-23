c8a510579faf706a9738ffbfc0bc733597b75f80
jls <jls@cordulus.com>
2024-12-23 12:38:36 +0100

    Solve 20.b

--- 20-python/main.py
+++ 20-python/main.py
@@ -90,3 +90,55 @@ for current_picoseconds, (x, y) in enumerate(path):
             res += 1
 
 print(f"Result: {res}")
+
+
+def check(
+    x: int,
+    y: int,
+    dist_map: list[list[int]],
+):
+    cost = dist_map[y][x]
+    cheats = set()
+    memory = {}
+    queue = [(x, y, 0)]
+    while len(queue) > 0:
+        x, y, steps = queue.pop()
+        if steps >= 20:
+            continue
+        next_steps = steps + 1
+
+        for dx, dy in (
+            (0, 1),
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+        ):
+            nx, ny = x + dx, y + dy
+
+            # Check if out of bounds
+            if nx < 0 or nx >= width or ny < 0 or ny >= height:
+                continue
+
+            # Check if we have visited this cell before in less steps
+            prev = memory.get((nx, ny), 30)
+            if prev <= next_steps:
+                continue
+            memory[(nx, ny)] = next_steps
+
+            # Check if is a solution that saves 100 picoseconds
+            if steps + dist_map[ny][nx] <= cost - 100:
+                cheats.add((nx, ny))
+
+            queue.append((nx, ny, next_steps))
+
+    return cheats
+
+
+res = 0
+for current_picoseconds, (x, y) in enumerate(path):
+    if current_picoseconds % 100 == 0:
+        print(f"Current: {current_picoseconds}")
+    cheats = check(x, y, dist_map)
+    res += len(cheats)
+
+print(res)
